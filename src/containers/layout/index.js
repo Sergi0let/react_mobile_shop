77431723e3fbe95bd7b3a1c8ab8a106cd61448d5
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-02-06 14:42:59 +0200

    add phones from pseudo API

--- src/containers/layout/index.js
+++ src/containers/layout/index.js
@@ -10,7 +10,16 @@ const routes = (
 )
 
 const Layout = () => {
-  return <div>{routes}</div>
+  return (
+    <div className="view-container">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">Sidebar</div>
+          <div className="col-md-9">{routes}</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Layout
