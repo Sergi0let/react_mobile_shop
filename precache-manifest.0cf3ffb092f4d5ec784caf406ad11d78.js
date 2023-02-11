a45b2aaa97de6d60425384a41d97b322e34e7847
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-02-11 15:10:36 +0200

    Updates

---
+++ precache-manifest.0cf3ffb092f4d5ec784caf406ad11d78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2f9f724bf7b9229ceb4cf7393bd4c9",
+    "url": "/Sergi0let/react_mobile_shop/index.html"
+  },
+  {
+    "revision": "8c6ec4f1a844a69ecca5",
+    "url": "/Sergi0let/react_mobile_shop/static/css/main.812602da.chunk.css"
+  },
+  {
+    "revision": "188af280344df4bd207b",
+    "url": "/Sergi0let/react_mobile_shop/static/js/2.17b25876.chunk.js"
+  },
+  {
+    "revision": "8c6ec4f1a844a69ecca5",
+    "url": "/Sergi0let/react_mobile_shop/static/js/main.85ecb968.chunk.js"
+  },
+  {
+    "revision": "b6f0f22de55a116e9032",
+    "url": "/Sergi0let/react_mobile_shop/static/js/runtime~main.99ce8cb5.js"
+  }
+]);
